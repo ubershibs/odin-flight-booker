d8c0e118c4a2e7540835696869e352ade8a19e83
ubershibs <luke.walker@gmail.coml>
2016-02-22 14:34:08 -0500

    Fix URL

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,8 +1,9 @@
 class PassengerMailer < ApplicationMailer
+  default from: "luke.walker@gmail.com"
 
   def thank_you(passenger)
     @passenger = passenger
-    @url = "https://secure-sands-80209.herokuapp.com/bookings/{@passenger.bookings.last.id}"
+    @url = "https://secure-sands-80209.herokuapp.com/bookings/#{ @passenger.bookings.last.id }"
     mail(to: @passenger.email, subject: "Thank you for your booking!")
   end
 end
