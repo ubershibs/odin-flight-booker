3898ea10ec3f89cf9b303b2071663e535eb36fcf
ubershibs <luke.walker@gmail.coml>
2016-02-22 13:10:57 -0500

    Upgrade to SSL and Puma

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -2,7 +2,7 @@ class PassengerMailer < ApplicationMailer
 
   def thank_you(passenger)
     @passenger = passenger
-    @url = "http://localhost:3000/bookings/#{@passenger.bookings.last.id}"
+    @url = "https://localhost:3000/bookings/#{@passenger.bookings.last.id}"
     mail(to: @passenger.email, subject: "Thank you for your booking!")
   end
 end
